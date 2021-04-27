7d94129de53881ab841fab792997519f223a2fc3
Jan Köster <jan.koester@bbaw.de>
2021-04-27 08:33:21 +0000

    DataConverter: more keys added

--- app/Http/Controllers/dbi/DataConverter.php
+++ app/Http/Controllers/dbi/DataConverter.php
@@ -24,11 +24,11 @@ class DataConverter {
 
     public function setContext ($entity) {
         return [
-            'dcterms'   => 'https://dublincore.org/specifications/dublin-core/dcmi-terms/',
+            'dcterms'   => 'https://www.dublincore.org/specifications/dublin-core/dcmi-terms/terms/',
             'foaf'      => 'http://xmlns.com/foaf/spec/',
             'nmo'       => 'http://nomisma.org/ontology#',
             'skos'      => 'https://www.w3.org/2009/08/skos-reference/skos.html#',
-            'schema'    => 'https://schema.org/'
+            //'schema'    => 'https://schema.org/'
         ];
     }
 
@@ -49,7 +49,7 @@ class DataConverter {
         $graph['dcterms:license'][0] = [
             '@id' => 'https://creativecommons.org/licenses/by/4.0',
             '@value' => 'CC BY 4.0',
-            'skos:note' => 'This only applies to the text data. The licence for other data types (e.g. images) is declared separately.'
+            'skos:note' => 'This licence applies to the jsonld-text data only! The respective licence for other resources (e.g. linked images) must be requested from the stated rights holder.'
         ];
 
         // Source
@@ -63,6 +63,14 @@ class DataConverter {
 
         // Name
         $graph['dcterms:title'][0] = ['@value' => 'cn '.rtrim($entity, 's').' '.$data['id']];
+
+        // Comment
+        if (!empty($data['comment']['en'])) $graph['skos:note'][] = ['@value' => $data['comment']['en']];
+        if (!empty($data['type_comment']['en'])) {
+            if (empty($graph['skos:note']) || $graph['skos:note'][0]['@value'] !== $data['type_comment']['en']) {
+                $graph['skos:note'][] = ['@value' => $data['type_comment']['en']];
+            }
+        }
 
         // Object Type
         $graph['nmo:'.($entity === 'types' ? 'represents' : 'has').'ObjectType'][0] = [
@@ -115,24 +123,6 @@ class DataConverter {
         }
         // Period??
 
-        // Material
-        if (!empty($data['material']['link'])) $graph['nmo:hasMaterial'][0] = [
-            '@id' => $data['material']['link'],
-            '@value' => empty($data['material']['text']['en']) ? $data['material']['text']['de'] : $data['material']['text']['en']
-        ];
-
-        // Denominaton
-        if (!empty($data['denomination']['link'])) $graph['nmo:hasDenomination'][0] = [
-            '@id' => $data['denomination']['link'],
-            '@value' => empty($data['denomination']['text']['en']) ? $data['denomination']['text']['de'] : $data['denomination']['text']['en']
-        ];
-
-        // Weight
-        if (!empty($data['weight']['value'])) $graph['nmo:hasWeight'][0] = [
-            '@value' => $data['weight']['value'],
-            'unit' => 'g'
-        ];
-
         // Diameter
         if (!empty($data['diameter']['value_max'])) $graph['nmo:hasDiameter']['nmo:hasMaxDiameter'][0] = [
             '@value' => $data['diameter']['value_max'],
@@ -143,18 +133,41 @@ class DataConverter {
             'unit' => 'mm'
         ];
 
+        // Weight
+        if (!empty($data['weight']['value'])) $graph['nmo:hasWeight'][0] = [
+            '@value' => $data['weight']['value'],
+            'unit' => 'g'
+        ];
+
+        // Material
+        if (!empty($data['material']['link'])) $graph['nmo:hasMaterial'][0]['@id'] = $data['material']['link'];
+        if (!empty($data['material']['text']['en'])) $graph['nmo:hasMaterial'][0]['@value'] = $data['material']['text']['en'];
+        else if (!empty($data['material']['text']['de'])) $graph['hasMaterial'][0]['@value'] = $data['material']['text']['de'];
+
+        // Denominaton
+        if (!empty($data['denomination']['link'])) $graph['nmo:hasDenomination'][0]['@id'] = $data['denomination']['link'];
+        if (!empty($data['denomination']['text']['en'])) $graph['nmo:hasDenomination'][0]['@value'] = $data['denomination']['text']['en'];
+        else if (!empty($data['denomination']['text']['de'])) $graph['nmo:hasDenomination'][0]['@value'] = $data['denomination']['text']['de'];
+
+        // Standard
+        if (!empty($data['standard']['link'])) $graph['nmo:hasWeightStandard'][0]['@id'] = $data['standard']['link'];
+        if (!empty($data['standard']['text']['en'])) $graph['nmo:hasWeightStandard'][0]['@value'] = $data['standard']['text']['en'];
+        else if (!empty($data['standard']['text']['de'])) $graph['nmo:hasWeightStandard'][0]['@value'] = $data['standard']['text']['de'];
+
         //nmo:hasManufacture (struck or cast)
         //nmo:hasProductionObject
             //nmo:hasDie
 
         // Axis
-        if (!empty($data['axis'])) $graph['nmo:hasAxis'][0] = ['@value' => $data['axis']];
-
-        //dcterms:Standard
-            //nmo:WeightStandard
+        if (!empty($data['axis']) || !empty($data['axes'])) {
+            foreach (empty($data['axes']) ? [$data['axis']] : $data['axes'] as $i => $a) {
+                $graph['nmo:hasAxis'][$i]['@value'] = $a['value'];
+            }
+        }
 
         // Appearance
         foreach(['obverse', 'reverse'] as $side) {
+            $gs = null;
             if (!empty($data[$side]['design']['text']['en'])) $gs['foaf:depiction'][0] = ['@value' => $data[$side]['design']['text']['en']];
             if (!empty($data[$side]['legend']['string'])) $gs['nmo:hasLegend'][0] = ['@value' => $data[$side]['legend']['string']];
 
@@ -184,30 +197,83 @@ class DataConverter {
                 //nmo:hasPortrait
                 //nmo:hasCountermark
 
-        // Images
-        //Tdcterms:rightsHolder
-
         // Pecularities
         if (!empty($data['pecularities']['en'])) $graph['nmo:Peculiarity'][0] = ['@value' => $data['pecularities']['en']];
 
-        //schema:Comment
 
-        //nmo:hasContext
-            //nmo:hasFindspot
-            //nmo:Find
-                //nmo:Hoard
+        // Context - Findspot
+        if (!empty($data['findspot']) || !empty($data['findspots'])) {
+            foreach (empty($data['findspots']) ? [$data['findspot']] : $data['findspots'] as $i => $fs) {
+                if (!empty($fs['link'])) $graph['nmo:hasContext']['nmo:hasFindspot'][$i]['@id'] = $fs['link'];
+                if (!empty($fs['name'])) $graph['nmo:hasContext']['nmo:hasFindspot'][$i]['@value'] = $fs['name'].($fs['country'] ? (' ('.strtoupper($fs['country']).')') : '');
+            }
+        }
+        // Context - Hoards
+        if (!empty($data['hoard']) || !empty($data['hoards'])) {
+            foreach (empty($data['hoards']) ? [$data['hoard']] : $data['hoards'] as $i => $hs) {
+                if (!empty($hs['link'])) $graph['nmo:hasContext']['nmo:Find']['nmo:Hoard'][$i]['@id'] = $hs['link'];
+                if (!empty($hs['name'])) $graph['nmo:hasContext']['nmo:Find']['nmo:Hoard'][$i]['@value'] = $hs['name'];
+            }
+        }
 
-        //nmo:hasReferenceWork (A published work of reference relevant to a numismatic object.)
-            //dcterms:BibliographicResource
-            //nmo:TypeSeries
+        // Owner
+        if (!empty($data['owner']['original']['link'])) $graph['nmo:hasCollection'][0]['@id'] = $data['owner']['original']['link'];
+        if (!empty($data['owner']['original']['name'])) $graph['nmo:hasCollection'][0]['@value'] = $data['owner']['original']['name'].
+        ($data['owner']['original']['city'] ? (', '.$data['owner']['original']['city']) : '').
+        ($data['owner']['original']['country'] ? (' ('.strtoupper($data['owner']['original']['country']).')') : '').
+        ($data['owner']['original']['collection_nr'] ? (', collection nr. '.$data['owner']['original']['collection_nr']) : '');
 
+        // References
+        $gr = [];
+        foreach (['citations', 'literature', 'type_literature'] as $references) {
+            if (!empty($data[$references])) {
+                foreach ($data[$references] as $refs) {
+                    if (!isset($gr[$refs['id']])) {
+                        $gr[$refs['id']] = [
+                            '@id' => $refs['link'],
+                            '@type' => 'https://www.dublincore.org/specifications/dublin-core/dcmi-terms/terms/BibliographicResource',
+                            '@value' => $refs['title'].(empty($refs['quote']['text']['en']) ? '' : (', '.$refs['quote']['text']['en'])),
+                        ];
+                        if (!empty($refs['quote']['comment']['en'])) $gr[$refs['id']]['schema:Comment'] = $refs['quote']['comment']['en'];
+                    }
+                }
+            }
+        }
+        if (!empty($gr)) $graph['nmo:hasReferenceWork'] = array_values($gr);
 
+        // Images
+        if (!empty($data['images'])) {
+            $i = 0;
+            foreach ($data['images'] as $img) {
+                foreach (['obverse', 'reverse'] as $side) {
+                    $link = $img[$side]['link'];
+                    if (!empty($link)) {
+                        if (substr($link, 0, 4) !== 'http') {
+                            $link = env('APP_URL').'/storage/'.ltrim(ltrim($link, '/'), 'storage/');
+                        }
 
-        //nmo:hasCollection
+                        $imgs[$i]['@id'] = $link;
 
-        //nmo:Controlmark ?
-        //nmo:Cauntermark ?
-        //nmo:Ethnic (tribe) ?
+                        //$rh = substr($link, 0, strlen(env('APP_URL'))) === env('APP_URL') ? null :
+
+                        $img_type = $img[$side]['kind'] === 'original' ? 'original' : 'reproduction';
+
+                        $imgs[$i]['skos:note'] = $img_type.', '.$side;
+
+                        if (!empty($img[$side]['photographer']) && !in_array($img[$side]['photographer'], ['privat', 'unbekannt'])) $imgs[$i]['dcterms:creator'][0]['@value'] = $img[$side]['photographer'];
+                        else if (!empty($data['owner'][$img_type]['name'])) $imgs[$i]['dcterms:creator'][0]['@value'] = $data['owner'][$img_type]['name'];
+
+                        if (!empty($data['owner'][$img_type]['link'])) $imgs[$i]['dcterms:rightsHolder'][0]['@id'] = $data['owner'][$img_type]['link'];
+                        if (!empty($data['owner'][$img_type]['name'])) $imgs[$i]['dcterms:rightsHolder'][0]['@value'] = $data['owner'][$img_type]['name'].
+                        ($data['owner'][$img_type]['city'] ? (', '.$data['owner'][$img_type]['city']) : '').
+                        ($data['owner'][$img_type]['country'] ? (' ('.strtoupper($data['owner'][$img_type]['country']).')') : '');
+
+                        ++$i;
+                    }
+                }
+            }
+        }
+        if (!empty($imgs)) $graph['dcterms:Image'] = $imgs;
 
         // ----------
         return $graph;
